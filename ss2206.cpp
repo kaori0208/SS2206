262b74e4be9c1b82f5966a2ccc11729504689860
kaori.yamamoto <parfum1103@outlook.jp>
2022-12-01 00:25:02 +0900

    ①終了

--- ss2206.cpp
+++ ss2206.cpp
@@ -10,30 +10,20 @@ using namespace std;
 // Objectクラス作成
 class Object{
     private:
-    // txtファイルの内容をtupple型で記録する
-    // vector<tupple<const string, string>> txtConfig = {{"Input",""},
-    //                                                  {"Output",""},
-    //                                                  {"ImageProcessing",""},
-    //                                                  {"WindowProcessing",""},
-    //                                                  {"WindowLevel",""}, 
-    //                                                  {"WindowWidth",""},
-    //                                                  {"MovingAverageFilterKernel",""},
-    //                                                  {"MedianFilterKernel",""}};
+    // txtファイルの内容をpair型のvectorで記録する。行ごとで後から追加していく
+    vector<pair<const string, string>> txtConfig;
     public:
-    // Object();
-    // ~Object();
+    Object() {};    // コンストラクタ
+    ~Object() {};   // ディストラクタ
     void LoadTxtFile(string file_name);
-    // LoadMhdFile();
+    // void LoadMhdFile();
 };
-// Object::Object(){}
-// Object::~Object(){}
 
 // LoadTxtFile()関数　txtファイルから情報を読み取る関数
 void Object::LoadTxtFile(string file_name){
 
     // テキストファイル読み込み
     ifstream reading_file;
-    vector<string> lines;
     string line;
     reading_file.open(file_name);
 
@@ -44,11 +34,19 @@ void Object::LoadTxtFile(string file_name){
     }
 
     // テキストファイルの情報を読み取る
-    while (getline(reading_file, line)){
-        lines.push_back(line);
-    }
-    for (const auto &i : lines){
-        cout << i << endl;
+    int i = 0;
+    while(getline(reading_file, line)){
+        int equal_pos = line.find("=");
+        line.erase(line.begin(), line.begin() + equal_pos + 2);
+        if(i == 0) {txtConfig.push_back(make_pair("Input", line));}                     // Input
+        if(i == 1) {txtConfig.push_back(make_pair("Output", line));}                    // Output
+        if(i == 2) {txtConfig.push_back(make_pair("ImageProcessing", line));}           // ImageProcessing
+        if(i == 3) {txtConfig.push_back(make_pair("WindowProcessing", line));}          // WindowProcessing
+        if(i == 4) {txtConfig.push_back(make_pair("WindowLevel", line));}               // WindowLevel
+        if(i == 5) {txtConfig.push_back(make_pair("WindowWidth", line));}               // WindowWidth
+        if(i == 6) {txtConfig.push_back(make_pair("MovingAverageFilterKernel", line));} // MovingAverageFilterKernel
+        if(i == 7) {txtConfig.push_back(make_pair("MedianFilterKernel", line));}        // MedianFilterKernel
+        i ++;
     }
     reading_file.close();
 }
@@ -72,6 +70,8 @@ int main(int argc, char *argv[]){
     string file_name(argv[1]);
     // 1. テキストファイルから必要な情報を取得
     obj.LoadTxtFile(file_name);
-
+    // for(int i = 0; i < 8; i ++ ){
+    //     cout << txtConfig.first << ", " << txtConfig.second << endl;
+    // }
     return 0;
 }
