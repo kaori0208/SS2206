93139bc45d26e9a3bb415370d286193a2c8492c0
kaori.yamamoto <parfum1103@outlook.jp>
2022-12-01 01:45:55 +0900

    ②appendできない

--- ss2206.cpp
+++ ss2206.cpp
@@ -9,14 +9,18 @@ using namespace std;
 
 // Objectクラス作成
 class Object{
-    private:
-    // txtファイルの内容をpair型のvectorで記録する。行ごとで後から追加していく
-    vector<pair<const string, string>> txtConfig;
     public:
+    // メンバ変数
+    // txtファイルの内容をpair型のvectorで記録する
+    vector<pair<const string, string>> txt_config;
+    // mhdファイルの内容をpair型のvectorで記録する
+    vector<pair<const string, string>> mhd_config;
+
+    // メンバ関数
     Object() {};    // コンストラクタ
     ~Object() {};   // ディストラクタ
     void LoadTxtFile(string file_name);
-    // void LoadMhdFile();
+    void LoadMhdFile(string& file_name);
 };
 
 // LoadTxtFile()関数　txtファイルから情報を読み取る関数
@@ -38,28 +42,34 @@ void Object::LoadTxtFile(string file_name){
     while(getline(reading_file, line)){
         int equal_pos = line.find("=");
         line.erase(line.begin(), line.begin() + equal_pos + 2);
-        if(i == 0) {txtConfig.push_back(make_pair("Input", line));}                     // Input
-        if(i == 1) {txtConfig.push_back(make_pair("Output", line));}                    // Output
-        if(i == 2) {txtConfig.push_back(make_pair("ImageProcessing", line));}           // ImageProcessing
-        if(i == 3) {txtConfig.push_back(make_pair("WindowProcessing", line));}          // WindowProcessing
-        if(i == 4) {txtConfig.push_back(make_pair("WindowLevel", line));}               // WindowLevel
-        if(i == 5) {txtConfig.push_back(make_pair("WindowWidth", line));}               // WindowWidth
-        if(i == 6) {txtConfig.push_back(make_pair("MovingAverageFilterKernel", line));} // MovingAverageFilterKernel
-        if(i == 7) {txtConfig.push_back(make_pair("MedianFilterKernel", line));}        // MedianFilterKernel
+        if(i == 0) {txt_config.push_back(make_pair("Input", line));}                     // Input
+        if(i == 1) {txt_config.push_back(make_pair("Output", line));}                    // Output
+        if(i == 2) {txt_config.push_back(make_pair("ImageProcessing", line));}           // ImageProcessing
+        if(i == 3) {txt_config.push_back(make_pair("WindowProcessing", line));}          // WindowProcessing
+        if(i == 4) {txt_config.push_back(make_pair("WindowLevel", line));}               // WindowLevel
+        if(i == 5) {txt_config.push_back(make_pair("WindowWidth", line));}               // WindowWidth
+        if(i == 6) {txt_config.push_back(make_pair("MovingAverageFilterKernel", line));} // MovingAverageFilterKernel
+        if(i == 7) {txt_config.push_back(make_pair("MedianFilterKernel", line));}        // MedianFilterKernel
+        if(i == 7){
+            for(const auto &[a, b]: txt_config){
+            cout << a << ", " << b << endl;
+            }
+        }
         i ++;
     }
     reading_file.close();
 }
 
 // LoadMhdFile()関数　mhdファイルから画像情報を読み取る関数
-// Object::LoadMhdFile(){
-//     return 0;
-// }
+void Object::LoadMhdFile(string& file_name){
+    cout << file_name << endl;
+}
 
 // メイン関数 -----------------------------------------------------
 int main(int argc, char *argv[]){
     
     Object obj;
+    string file_name(argv[1]);
 
     // コマンド引数　例外処理
     if(argc < 2){
@@ -67,11 +77,11 @@ int main(int argc, char *argv[]){
         exit(-1);
     }
 
-    string file_name(argv[1]);
     // 1. テキストファイルから必要な情報を取得
     obj.LoadTxtFile(file_name);
-    // for(int i = 0; i < 8; i ++ ){
-    //     cout << txtConfig.first << ", " << txtConfig.second << endl;
-    // }
+    // 2. mhdファイルから画像情報を取得
+    cout << obj.txt_config[0].second << endl;
+    
+    // obj.LoadMhdFile(obj.txt_config[0].second);
     return 0;
 }
